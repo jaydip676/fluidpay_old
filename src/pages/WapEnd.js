b185f36f841eed6a4d6cb962d87f3783b138a51c
sarvagna <sarvagnakadiya1@gmail.com>
2022-12-08 15:59:33 +0530

    changes in end-stream files

--- src/pages/WapEnd.js
+++ src/pages/WapEnd.js
@@ -14,34 +14,33 @@ function WapEnd() {
     if (dataFetchedRef.current) return;
     dataFetchedRef.current = true;
 
+    const endStream = async () => {
+      console.log("getting");
+      const platform = "0xbFc4A28D8F1003Bec33f4Fdb7024ad6ad1605AA8";
+      const user = "0xcc4091815292B2D3BB3076022Dc72d432B6cAdEb";
+      if (typeof window.ethereum !== "undefined") {
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        await provider.send("eth_requestAccounts", []);
+        const signer = provider.getSigner();
+        console.log("inside id");
+        console.log(quickPay_abi);
+
+        const connectedContract = new ethers.Contract(
+          CONTRACT_ADDRESS,
+          quickPay_abi,
+          signer
+        );
+        console.log("wait...");
+        let tx = await connectedContract.deleteStream(user, platform, {
+          gasLimit: 500000,
+        });
+        console.log(tx);
+        setData("Your stream is ended. Have a nice day");
+        timeBeenStreamed();
+      }
+    };
     endStream();
   }, []);
-
-  const endStream = async () => {
-    console.log("getting");
-    const platform = "0xbFc4A28D8F1003Bec33f4Fdb7024ad6ad1605AA8";
-    const user = "0xcc4091815292B2D3BB3076022Dc72d432B6cAdEb";
-    if (typeof window.ethereum !== "undefined") {
-      const provider = new ethers.providers.Web3Provider(window.ethereum);
-      await provider.send("eth_requestAccounts", []);
-      const signer = provider.getSigner();
-      console.log("inside id");
-      console.log(quickPay_abi);
-
-      const connectedContract = new ethers.Contract(
-        CONTRACT_ADDRESS,
-        quickPay_abi,
-        signer
-      );
-      console.log("wait...");
-      let tx = await connectedContract.deleteStream(user, platform, {
-        gasLimit: 500000,
-      });
-      console.log(tx);
-      setData("Your stream is ended. Have a nice day");
-      timeBeenStreamed();
-    }
-  };
 
   const timeBeenStreamed = async () => {
     console.log("getting");
